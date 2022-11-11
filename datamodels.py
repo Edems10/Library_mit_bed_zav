e883e5667946cc320b80310e437affa70d3c7426
Adam Mitrenga <edemsikk@gmail.com>
2022-11-11 13:50:23 +0100

    UseUserrUseUseUserrrUseg

--- datamodels.py
+++ datamodels.py
@@ -1,14 +1,11 @@
-from lib2to3.pgen2.token import OP
-import uuid
 from dataclasses import dataclass, field
 from datetime import datetime
-from marshmallow_dataclass import dataclass as mm_dataclass
-from typing import Optional, List, Union
-from dataclasses_json import dataclass_json, Undefined
-from marshmallow import validate
 from enum import Enum
+from typing import Optional
 import bcrypt
 from bson.objectid import ObjectId
+from dataclasses_json import Undefined, dataclass_json
+from marshmallow import validate
 
 
 class Roles(Enum):
@@ -30,28 +27,24 @@ class Autocomplete_options_user(Enum):
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class Book:
-    # id is optional because it is added by mongodb at the time of creation
-    # _id: Optional[str]
+    _id: ObjectId
     title: str = field(metadata={"validate": validate.Length(min=1, max=256)})
     author: str = field(metadata={"validate": validate.Length(min=1, max=256)})
     length: int
     year: int
+    #FIXME toto muze byt i sama classa o sobe 
+    #zalezi potom jak budeme ukladat images
     image: str
-    # idk mby we want to save text as well
-    # text : str
-    #  current_borrowers: Optional[dict] #{Person_id:current_time}
     copies_available: int
     genre: Optional[str]
     description: Optional[str]
     count_borrowed: int
-    # mongo gives id to each object by default so this might not be needed
-    # id: str = field(default_factory=lambda: str(uuid.uuid4())[:8])
 
 
 @dataclass_json(undefined=Undefined.EXCLUDE)
 @dataclass
 class Person:
-    _id: Optional[str]
+    _id: ObjectId
     first_name: str = field(metadata={"validate": validate.Length(min=1, max=256)})
     surname: str = field(metadata={"validate": validate.Length(min=1, max=256)})
     pid: int
@@ -59,14 +52,10 @@ class Person:
     login_name: str = field(metadata={"validate": validate.Length(min=1, max=32)})
     password: str = field(metadata={"validate": validate.Length(min=6, max=64)})
     salt: str = bcrypt.gensalt()
-    borrowed_books = Optional[dict]  # {Book}
-    history_of_books = Optional[dict]  # {Book_id: object(sdafasdfasf),time_returned_at : 139025925.23}
     count_borrowed_books: int = 0
-    stashed_changes = Optional[dict]
     banned: bool = False
     verified: bool = False
     approved_by_librarian: bool = True
-
     role: Roles = Roles.User
     created_at: datetime = field(metadata={
         'dataclasses_json': {
@@ -80,3 +69,30 @@ class Person:
     @property
     def id(self):
         return self._id
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass    
+class Book_status:
+    book_id: str
+    user_id: str
+    date_borrowed: datetime
+    date_returned: datetime
+    returned :bool
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass    
+class Person_changes:
+    person_id: ObjectId
+    first_name: str = field(metadata={"validate": validate.Length(min=1, max=256)})
+    surname: str = field(metadata={"validate": validate.Length(min=1, max=256)})
+    address: str = field(metadata={"validate": validate.Length(min=1, max=256)})
+    #FIXME 
+    # teoreticky bych rekl ze to tam nemusi vubec byt kdyz ten doc
+    # potom budeme delovat
+    approved: bool
+    created_at: datetime = field(metadata={
+        'dataclasses_json': {
+            'encoder': lambda x: datetime.timestamp(x),
+        }
+    }, default_factory=datetime.utcnow)
+    
